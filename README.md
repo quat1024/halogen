52e2488fa515d1a6a12c9301faf33bb12417b568
quat1024 <quat1024@users.noreply.github.com>
2021-06-14 02:24:42 -0400

    initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Halogen
+
+magic mod
---
+++ src/main/java/agency/highlysuspect/halogen/Init.java
@@ -0,0 +1,10 @@
+package agency.highlysuspect.halogen;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Init implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		
+	}
+}
